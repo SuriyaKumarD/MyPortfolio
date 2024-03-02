abb9ff8ada01a24455ddae31cf9f1f015b0dc0af
suriya <suriyakumar.d@gmail.com>
2024-03-03 01:46:01 +0530

    Portfolio Changes Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeaderComponent } from './dashboard/header/header.component';
+import { AboutComponentComponent } from './dashboard/header/about-component/about-component.component';
+import { SkillComponent } from './dashboard/header/skill/skill.component';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, HeaderComponent],
+  declarations: [AppComponent, DashboardComponent, HeaderComponent, AboutComponentComponent, SkillComponent],
   imports: [BrowserModule, AppRoutingModule, NgxTypedJsModule],
   providers: [],
   bootstrap: [AppComponent],
